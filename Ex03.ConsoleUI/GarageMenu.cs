3ed6777db5b77ed31847a879da19553ab17ff1c5
shakedrobinzon <shakedrobinzon@gmail.com>
2022-05-19 16:45:05 +0300

    1

---
+++ Ex03.ConsoleUI/GarageMenu.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Ex03.ConsoleUI
+{
+    public class GarageMenu
+    {
+        public GarageMenu()
+        {
+        }
+    }
+}
---
+++ Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace B22Ex03Shaked203943253Fanny337957633
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Engine test1 = new ElectricEngine(10,5);
+            Engine test2 = new FuelEngine((e_FuelType)1, 10, 5);
+        }
+    }
+}
