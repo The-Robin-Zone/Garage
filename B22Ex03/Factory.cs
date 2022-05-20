c9738c438b96268a2370d5d074f6d5096435088f
shakedrobinzon <shakedrobinzon@gmail.com>
2022-05-20 10:32:11 +0300

    1

--- B22Ex03/Factory.cs
+++ B22Ex03/Factory.cs
@@ -6,52 +6,51 @@ namespace B22Ex03Shaked203943253Fanny337957633
 
     public static class Factory
     {
-        internal static Vehicle CreateNewVehicle(string i_ModelName, string i_LicenseNumber,
-            e_VehicleType i_VehicleType, e_EngineType i_EngineType, e_FuelType? i_FuelType, float i_CurrentEnergyCapcity,
-            float i_MaxEnergy, string i_WheelManufactor, float i_CurrentAirPressure, float i_MaxAirPressure)
+        internal static Vehicle CreateNewVehicle(string i_LicenseNumber,
+            e_VehicleType i_VehicleType, e_EngineType i_EngineType, e_FuelType? i_FuelType)
         {
+
             Vehicle newVehicle = null;
-            Engine engine = CreateEngine(i_EngineType, i_FuelType, i_MaxEnergy, i_CurrentEnergyCapcity);
-            Wheel wheel = new Wheel(i_MaxAirPressure);
+            Engine newEngine = CreateEngine(i_EngineType, i_FuelType);
+
 
             switch (i_VehicleType)
             {
                 case e_VehicleType.Car:
-                    newVehicle = new Car(i_LicenseNumber);
+                    newVehicle = new Car(i_LicenseNumber, newEngine);
                     break;
 
                 case e_VehicleType.Motorcycle:
-                    newVehicle = new Motorcycle(i_LicenseNumber);
+                    newVehicle = new Motorcycle(i_LicenseNumber, newEngine);
                     break;
 
                 case e_VehicleType.Truck:
-                    newVehicle = new Truck(i_LicenseNumber);
+                    newVehicle = new Truck(i_LicenseNumber, newEngine);
                     break;
             }
 
             return newVehicle;
         }
 
-        private static Engine CreateEngine(e_EngineType i_EngineType, e_FuelType? i_FuelType,
-            float i_MaxEnergy, float i_CurrentEnergyCapcity)
+        private static Engine CreateEngine(e_EngineType i_EngineType, e_FuelType? i_FuelType)
         {
-            Engine engine;
+            Engine newEngine = null;
             switch (i_EngineType)
             {
                 case e_EngineType.Fuel:
-                    engine = new FuelEngine((e_FuelType)i_FuelType, i_MaxEnergy);
+                    newEngine = new FuelEngine((e_FuelType)i_FuelType);
                     break;
 
                 case e_EngineType.Electric:
-                    engine = new ElectricEngine(i_MaxEnergy);
+                    newEngine = new ElectricEngine();
                     break;
 
                 default:
-                    engine = null;
+                    newEngine = null;
                     break;
             }
 
-            return engine;
+            return newEngine;
         }
     }
 }
--- B22Ex03/Program.cs
+++ B22Ex03/Program.cs
@@ -5,8 +5,19 @@ namespace B22Ex03Shaked203943253Fanny337957633
     {
         public static void Main()
         {
-            GarageMenu newGarage = new GarageMenu();
-            newGarage.OpenGarageMenu();
+            //GarageMenu newGarage = new GarageMenu();
+            //newGarage.OpenGarageMenu();
+
+
+            string i_LicenseNumber = "1992";
+            e_VehicleType i_VehicleType = e_VehicleType.Car;
+            e_EngineType i_EngineType = e_EngineType.Fuel;
+            e_FuelType? i_FuelType = e_FuelType.Octane95;
+
+
+            Vehicle shaked = Factory.CreateNewVehicle(i_LicenseNumber, i_VehicleType, i_EngineType, i_FuelType);
+
+            Console.WriteLine(shaked.ToString());
         }
     }
 }
